e968d41359b299156484f04694d6eba29bc2e38e
wxxk <dwde22@gmail.com>
2023-08-14 17:06:01 +0900

    First commit

---
+++ K-Content/src/main/java/com/example/myapp/KContentApplication.java
@@ -0,0 +1,13 @@
+package com.example.myapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KContentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KContentApplication.class, args);
+	}
+
+}

ad7cca2436dbd1ee0ced8848a97466969b4ff07b
jungyeol <jungyeoly1@naver.com>
2023-08-25 17:03:18 +0900

    2023-08-25 회원가입/로그인 구현(진행 중)

---
+++ K-Content/src/main/java/com/example/myapp/mber/service/MberService.java
@@ -0,0 +1,42 @@
+package com.example.myapp.mber.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.myapp.mber.dao.IMberRepository;
+import com.example.myapp.mber.model.Mber;
+
+@Service
+public class MberService implements IMberService {
+
+	@Autowired
+	IMberRepository mberRepository;
+	
+	@Override
+	public Mber selectMberbyId(String mberId) {
+		return mberRepository.selectMberbyId(mberId);
+	}
+
+	@Override
+	public List<Mber> selectMberAllList() {
+		return mberRepository.selectMberAllList();
+	}
+
+	@Override
+	public void insertMber(Mber mber) {
+		mberRepository.insertMber(mber);
+	}
+
+	@Override
+	public void updateMber(Mber mber) {
+		mberRepository.updateMber(mber);
+	}
+
+	@Override
+	public void deleteMber(String mberId) {
+		mberRepository.deleteMber(mberId);
+	}
+
+}

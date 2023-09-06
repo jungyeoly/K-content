fc3793d39898c42809c5f801de0a5a9207e89998
jungyeol <jungyeoly1@naver.com>
2023-09-06 09:07:35 +0900

    2023-09-06 회원가입/로그인 오류 수정

--- K-Content/src/main/java/com/example/myapp/user/mber/controller/MberController.java
+++ K-Content/src/main/java/com/example/myapp/user/mber/controller/MberController.java
@@ -104,16 +104,16 @@ public class MberController {
 
 	@RequestMapping(value = "/mber/signup", method = RequestMethod.POST)
 	public String signup(Mber mber, HttpSession session, Model model) {
-//		System.out.println(mber.getMberId());
-//		System.out.println(mber.getMberPwd());
-//		System.out.println(mber.getMberEmail());
-//		System.out.println(mber.getMberName());
-//		System.out.println(mber.getMberBirth());
-//		System.out.println(mber.getMberPhone());
-//		System.out.println(mber.getMberRegistDate());
-//		System.out.println(mber.getMberUpdateDate());
-//		System.out.println(mber.getMberGenderCode());
-//		System.out.println(mber.getMberStatCode());
+		System.out.println(mber.getMberId());
+		System.out.println(mber.getMberPwd());
+		System.out.println(mber.getMberEmail());
+		System.out.println(mber.getMberName());
+		System.out.println(mber.getMberBirth());
+		System.out.println(mber.getMberPhone());
+		System.out.println(mber.getMberRegistDate());
+		System.out.println(mber.getMberUpdateDate());
+		System.out.println(mber.getMberGenderCode());
+		System.out.println(mber.getMberStatCode());
 		try {
 			mberService.insertMber(mber);
 		} catch (DuplicateKeyException e) {
@@ -151,17 +151,22 @@ public class MberController {
 	public String sendTempPwd(@RequestParam String mberId, @RequestParam String mberEmail) throws Exception {
 
 		Mber mber = mberService.selectMberbyIdEmail(mberId, mberEmail);
-		String dbMberId = mber.getMberId();
-		String dbMberEmail = mber.getMberEmail();
-		
-		logger.info(dbMberId);
-		logger.info(dbMberEmail);
+		logger.info(mber.getMberId());
+		logger.info(mber.getMberEmail());
+
+		logger.info(mber.getMberBirth());
+		logger.info(mber.getMberUpdateDate());
+		logger.info(mber.getMberPhone());
+		logger.info(mber.getMberName());
+//		String dbMberId = mber.getMberId();
+//		String dbMberEmail = mber.getMberEmail();
 		String tempPwd = emailService.sendTempPwd(mberEmail);
-		logger.info("임시비밀번호 : " + tempPwd);
+		System.out.println(tempPwd);
 		// 회원 정보 업데이트
 		if (mber != null) {
-		mber.setMberPwd(tempPwd);
-		mberService.updateMber(mber);
+			mber.setMberPwd(tempPwd);
+			logger.info(mber.getMberPwd());
+			mberService.updateMber(mber);
 		
 		} 
 		

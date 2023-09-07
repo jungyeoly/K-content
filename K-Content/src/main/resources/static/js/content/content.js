fef23a1ea3e10b00a88c9104e7ab7e0108942c71
wxxk <dwde22@gmail.com>
2023-09-07 14:23:20 +0900

    main search 기능 추가

--- K-Content/src/main/resources/static/js/content/content.js
+++ K-Content/src/main/resources/static/js/content/content.js
@@ -8,21 +8,42 @@ $(document).ready(function() {
 			layout.append(data);
 		}
 	})
-})
-	
-$(".cate").click(function() {
-	var cateValue = this.getAttribute('data-maincate-value');
-    console.log('선택한 cate 값: ' + cateValue);
-    
-    $.ajax({
-    	url: "/user/content",
-    	type: "GET",
-    	data: { cate: cateValue },
-    	success: function(data) {
-			let layout = $(".layout");
-			layout.find(".container").remove();
-			layout.append(data);
-    	}
-    })
-})
 
+	
+	$(".cate").click(function() {
+		var cateValue = this.getAttribute('data-maincate-value');
+		    
+		if (cateValue == null) {
+			cateValue = 'All';
+		}
+		
+	    $.ajax({
+	    	url: "/user/content",
+	    	type: "GET",
+	    	data: { cate: cateValue },
+	    	success: function(data) {
+				let layout = $(".layout");
+				layout.find(".container").remove();
+				layout.append(data);
+	    	}
+	    })
+	})
+
+
+	$("#searchForm").submit(function(event) {
+		event.preventDefault();
+		    
+		var keyword = $("#searchInput").val();
+		$.ajax({
+			url: "/user/content",
+			type: "POST",
+			data: { keyword : keyword },
+			success: function(data) {
+				let layout = $(".layout");
+				layout.find(".container").remove();
+				layout.append(data);
+			}
+		})
+	});
+	
+})

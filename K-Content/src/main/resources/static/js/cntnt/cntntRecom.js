2b7141c458570ce20066f202d0afdcc2eb9b8432
hesu <jhs60110@naver.com>
2023-09-14 12:18:52 +0900

    유튜브 API X 콘텐츠 생성

--- K-Content/src/main/resources/static/js/cntnt/cntntRecom.js
+++ K-Content/src/main/resources/static/js/cntnt/cntntRecom.js
@@ -10,21 +10,16 @@ $(document).ready(function () {
                 arr = Array.from(contentList[i].title);
                 arr.map((data,idx) => {
                     if(data == "'"){
-                        //arr[idx] = "\\'";
                         str = str + "\\'";
                     }
                     else if(data == '"'){
-                        //arr[idx] = '\\"';
                         str = str + '\\"';
                     }
                     else{
                         str = str + data;
                     }
                 })
-                // spaArr =arr.join('');
 
-
-                // console.log("spaArrspaArrspaArr "+str);
                 inHtml = `
             <li class="card-item" id="card-item" onclick="cntntMake( '${str}' , '${contentList[i].url}' )">
                 <figure class="card-image"style="background-image: url(${contentList[i].thumbnail})">
@@ -43,45 +38,22 @@ $(document).ready(function () {
     });
 })
 
-
 function cntntMake(spaArr,url) {
-console.log("spaArr: "+ spaArr);
-    console.log("url: "+ url);
-    // JSON.stringify(contentList);
-    // console.log(JSON.stringify(contentList));
-    // arr = Array.from(cntntTitle);
-    // index = [];
-    //
-    // for (i = 0; i < arr.length; i++) {
-    //     if (arr[i] == "\"" || arr[i] == "\'") {
-    //         index.push(i);
-    //     }
-    // }
-    //
-    // for (i = 0; i < index.length; i++) {
-    //     if (index[i] == 0) {
-    //         arr.unshift('\\');
-    //     } else {
-    //         arr.splice(i, 0, '\\');
-    //     }
-    // }
+    const formHtml = `
+                    <form id="contentMake" action="/cs/makecontent" method="get">
+                        <input  id="cntntURL" name="cntntURL"  />
+                        <input  id="cntntTitle" name="cntntTitle"  />
+                    </form>`;
 
-    // console.log("arr" +arr);
-    // const formHtml = `
-    //                 <form id="contentMake" action="/cs/makecontent" method="get">
-    //                     <input  id="cntntURL" name="cntntURL"  />
-    //                     <input  id="cntntTitle" name="cntntTitle"  />
-    //                 </form>`;
-    //
-    // const doc = new DOMParser().parseFromString(formHtml, 'text/html');
-    // const form = doc.body.firstChild;
-    // document.body.append(form);
-    // document.getElementById("cntntURL").value = cntntURL;
-    // document.getElementById("cntntTitle").value = cntntTitle;
-    // ;
-    // console.log(document.getElementsByClassName("cntntURL").value);
-    // console.log(document.getElementsByClassName("cntntTitle").value);
-    // document.getElementById('contentMake').submit();
+    const doc = new DOMParser().parseFromString(formHtml, 'text/html');
+    const form = doc.body.firstChild;
+    document.body.append(form);
+    document.getElementById("cntntURL").value = url;
+    document.getElementById("cntntTitle").value = spaArr;
+    ;
+    console.log(document.getElementsByClassName("cntntURL").value);
+    console.log(document.getElementsByClassName("cntntTitle").value);
+    document.getElementById('contentMake').submit();
 }
 
 function searchButton() {

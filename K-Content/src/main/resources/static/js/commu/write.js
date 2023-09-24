0f2b23c8f459e44f9a78930b765214343dae1b42
윤정아 <yja01270@naver.com>
2023-09-24 23:24:38 +0900

    유효성처리 진행중

--- K-Content/src/main/resources/static/js/commu/write.js
+++ K-Content/src/main/resources/static/js/commu/write.js
@@ -29,34 +29,29 @@ function displayFileList() {
 }
 
 function validateForm() {
-	console.log('validateForm 실행');
 	var title = document.getElementById('title').value;
 	var category = document.getElementById('category').value;
 	var cntnt = document.getElementById('cntnt').value;
-	console.log(category, title, cntnt); // 값이 제대로 설정되었는지 확인
 
 	if (!category || category.trim() === "") {
-		console.log('Category is empty!'); // 카테고리가 비어있는지 확인
 		showModal("K-Spectrum", "카테고리를 선택하세요.");
 		return false;
 	}
 
 	if (!title || title.trim() === "") {
-		console.log('Title is empty!'); // 제목이 비어있는지 확인
 		showModal("K-Spectrum", "제목을 입력하세요.");
 		return false;
 	}
 
 	if (!cntnt || cntnt.trim() === "") {
-		console.log('Content is empty!'); // 내용이 비어있는지 확인
 		showModal("K-Spectrum", "내용을 입력하세요.");
 		return false;
 	}
 	// 유효성 검사를 통과한 경우 모달을 보여줍니다.
 	 showModal('정말로 등록하시겠습니까?', '등록을 원하시면 확인을, 취소하시면 취소를 눌러주세요.', function() {
-      document.querySelector('form').submit();
-    });
-  }
+        document.querySelector('form').submit();
+   }, true);
+}
 
 document.querySelector('form').addEventListener('submit', function(event) {
 	event.preventDefault(); // 폼의 기본 제출 동작을 막습니다.
@@ -70,37 +65,54 @@ document.querySelector('.cancel-btn').addEventListener('click', function(event) 
     
     let isFormFilled = false;
     document.querySelectorAll('#category, #title, #cntnt').forEach(element => {
+	  console.log("콜백 함수가 호출되었습니다.");  // 이 로그가 출력되는지 확인
         if (element.value.trim() !== '') isFormFilled = true;
     });
 
     if (isFormFilled) {
-        showModal('취소 확인', '입력한 내용이 모두 사라집니다. 정말로 취소하시겠습니까?', function() {
-            // '확인' 버튼이 눌렸을 때 실행될 콜백 함수
-            document.querySelector('form').reset();  // 폼을 리셋합니다.
-            var commonModal = bootstrap.Modal.getInstance(document.getElementById('commonModal'));
-            if (commonModal) commonModal.hide();  // 모달을 숨깁니다.
-        });
+    showModal('취소 확인', '입력한 내용이 모두 사라집니다. 정말로 취소하시겠습니까?', function() {
+        document.querySelector('form').reset();
+        var commonModal = bootstrap.Modal.getInstance(document.getElementById('commonModal'));
+        if (commonModal) {
+            commonModal.hide();
+        }
+    });
     } else {
         window.location.href = '/commu';  // 메인 페이지로 바로 이동합니다.
     }
 });
 
 
-function showModal(title, content, callback) {
-  document.getElementById('commonModalLabel').textContent = title;
-  document.querySelector('.modal-body').textContent = content;
-  var commonModal = new bootstrap.Modal(document.getElementById('commonModal'));
+function showModal(title, content, callback, isSubmit = false) {
+	console.log("콜백:", callback); 
+    document.getElementById('commonModalLabel').textContent = title;
+    document.querySelector('.modal-body').textContent = content;
+    var commonModal = new bootstrap.Modal(document.getElementById('commonModal'));
 
-  const confirmButton = document.querySelector('.modal-footer .btn-confirm');
-  if (confirmButton) {
-    confirmButton.onclick = function() {
-      if (callback) callback();
-      commonModal.hide();
+    // isSubmit이 true인 경우 취소 버튼 숨기기
+    const cancelButton = document.querySelector('.modal-footer .btn-secondary');
+    if (isSubmit && cancelButton) {
+    } else {
+        cancelButton.style.display = 'block';
     }
-  }
 
-  commonModal.show();
+    const confirmButton = document.querySelector('.modal-footer .btn-confirm');
+    if (confirmButton) {
+        // 기존의 모든 이벤트 리스너를 제거
+        let newConfirmBtn = confirmButton.cloneNode(true);
+        confirmButton.parentNode.replaceChild(newConfirmBtn, confirmButton);
+
+        // 새로운 콜백으로 이벤트 리스너를 설정
+        newConfirmBtn.onclick = function() {
+	console.log("확인 버튼 클릭됨.");  // 이 부분 추가
+            if (callback) callback();
+            commonModal.hide();
+        };
+    }
+
+    commonModal.show();
 }
+
 
 
 document.getElementById('category').addEventListener('change', function() {
@@ -110,18 +122,8 @@ document.getElementById('category').addEventListener('change', function() {
 });
 
 window.addEventListener('DOMContentLoaded', (event) => {
-	if (document.getElementById('commuWritePage')) {
-		const cancelButton = document.querySelector('.modal-footer .btn-secondary');
-		if (cancelButton) cancelButton.style.display = 'none';
-	}
-
-	const confirmButton = document.querySelector('.modal-footer .btn-confirm');
-	if (confirmButton) {
-		confirmButton.addEventListener('click', function() {
-			var commonModal = bootstrap.Modal.getInstance(document.getElementById('commonModal'));
-			if (commonModal) commonModal.hide();
-		});
-	}
+    if (document.getElementById('commuWritePage')) {
+        const cancelButton = document.querySelector('.modal-footer .btn-secondary');
+        if (cancelButton) cancelButton.style.display = 'block';  
+    }
 });
-
-

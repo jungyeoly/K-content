f3a24f465dc72ef01e634920085cce5a207751cc
윤정아 <yja01270@naver.com>
2023-09-29 04:18:01 +0900

    페이이  현재 바뀌는 ㅁ누제

--- K-Content/src/main/resources/static/js/commu/commulist.js
+++ K-Content/src/main/resources/static/js/commu/commulist.js
@@ -1,110 +1,123 @@
 $(document).ready(function() {
-	let currentPage = 1; // 페이지 초기화
+    let currentPage = 1; // 페이지 초기화
+    let loading = false; // 페이지 로딩 중 여부를 나타내는 플래그
 
-	// 페이지 번호 클릭 이벤트 (이전/다음 버튼 제외)
-	$(document).on('click', '.page-link.selpage', function(e) {
-		e.preventDefault();
-		const selectedPage = $(this).data('selpage');
-		loadPage(selectedPage);
-	});
+    loadPage(currentPage);
 
-	// 이전 페이지 버튼 클릭 이벤트
-	$(document).on('click', '.page-link.prepage', function(e) {
-		e.preventDefault();
-		loadPage(currentPage - 1);
-	});
+    // 페이지 번호 클릭 이벤트
+    $(document).on('click', '.page-link', function(e) {
+        e.preventDefault();
+        if (!loading) { // 페이지 로딩 중이 아닐 때만 처리
+            const selectedPage = $(this).data('page');
+            if (selectedPage !== currentPage) {
+                loadPage(selectedPage);
+            }
+        }
+    });
 
-	// 다음 페이지 버튼 클릭 이벤트
-	$(document).on('click', '.page-link.nextpage', function(e) {
-		e.preventDefault();
-		loadPage(currentPage + 1);
-	});
+    // 이전 페이지 버튼 클릭 이벤트
+    $(document).on('click', '.page-link.prepage', function(e) {
+        e.preventDefault();
+        if (!loading && currentPage > 1) {
+            loadPage(currentPage - 1);
+        }
+    });
 
-	// 메인 페이지
-	function loadPage(page) {
-		$.ajax({
-			url: `/commu/ajax/${page}`,
-			method: 'GET',
-			dataType: 'json',
-			cache: false,
-			success: function(response) {
-				updatePostList(response.commulist);
-				updatePagination(response.nowPage, response.totalPageCount);
-				currentPage = page; // 현재 페이지 업데이트
-			},
-			error: function(error) {
-				console.error("Failed to load posts:", error);
-			}
-		});
-	}
+    // 다음 페이지 버튼 클릭 이벤트
+    $(document).on('click', '.page-link.nextpage', function(e) {
+        e.preventDefault();
+        if (!loading && currentPage < totalPageCount) {
+            loadPage(currentPage + 1);
+        }
+    });
 
-	// 카테고리 클릭 이벤트
+    // 메인 페이지
+    function loadPage(page) {
+        if (typeof page === 'undefined' || !page) {
+            page = 1;
+        }
+
+        loading = true; // 페이지 로딩 중 플래그 설정
+        $.ajax({
+            url: `/commu/ajax/${page}`,
+            method: 'GET',
+            dataType: 'json',
+            cache: false,
+            success: function(response) {
+                updatePostList(response.commulist);
+                updatePagination(response.nowPage, response.totalPageCount);
+                currentPage = page;
+            },
+            error: function(error) {
+                console.error("Failed to load posts:", error);
+            },
+            complete: function() {
+                loading = false; // 페이지 로딩 완료 시 플래그 해제
+            }
+        });
+    }
+
+
+// 카테고리 클릭 이벤트
 $(document).on('click', '.cate', function() {
     const commuCateCode = $(this).data('maincate-value');
     loadCategoryPosts(commuCateCode, 1); // 페이지를 1페이지로 초기화하여 로드
 });
 
-	// "All" 카테고리 선택
-	$(document).on('click', '.all-cate', function() {
-		loadPage(1); // 페이지 초기화하여 첫 번째 페이지를 로드
-	});
-
-	function loadCategoryPosts(commuCateCode, page) {
-		 $.ajax({
+// 카테고리 클릭 이벤트
+function loadCategoryPosts(commuCateCode, page) {
+    $.ajax({
         url: `/commu/commucatecode/${commuCateCode}?page=${page}`,
         type: 'GET',
         dataType: 'json',
         success: function(response) {
             updatePostList(response.posts);
-            // 클라이언트 측에서 페이지 번호 계산 및 업데이트
-            updatePagination(page, Math.ceil(response.posts.length / 10));
+            updatePagination(response.nowPage, response.totalPageCount);
+            currentPage = page; // currentPage를 업데이트하여 페이지 번호를 유지
         },
         error: function(error) {
             console.error("Error fetching posts by category:", error);
         }
     });
 }
-// 페이지 목록을 동적으로 생성하는 함수
+
+	// 페이지 목록을 동적으로 생성하는 함수
 function updatePagination(nowPage, totalPageCount) {
     let paginationHtml = '';
-
     for (let i = 1; i <= totalPageCount; i++) {
-        if (i === nowPage) {
-            paginationHtml += `<li class="page-item active"><a class="page-link selpage" data-selpage="${i}">${i}</a></li>`;
-        } else {
-            paginationHtml += `<li class="page-item"><a class="page-link selpage" data-selpage="${i}">${i}</a></li>`;
-        }
+        const pageLinkClass = i === nowPage ? 'page-item active' : 'page-item';
+        paginationHtml += `
+            <li class="${pageLinkClass}">
+                <a class="page-link" data-page="${i}">${i}</a>
+            </li>
+        `;
     }
-
-    $('.pagination').html(paginationHtml);
+    
+    const paginationElement = document.querySelector('.pagination'); // 페이지 목록을 표시할 요소 선택
+    if (paginationElement) {
+        paginationElement.innerHTML = paginationHtml; // 페이지 목록을 요소에 할당
+    }
 }
 
+
+
+
+
 	function updatePostList(posts) {
-		if (!Array.isArray(posts)) {
-			console.error('Provided posts data is not an array:', posts);
-			return; // Exit the function early
-		}
-
-		
-
 		let postListHtml = '';
 		posts.forEach(commu => {
 			postListHtml += `
-            <tr class="commu-row" data-commu-id="${commu.commuId}">
-                <td>${commu.commuId}</td>
-                <td>${commu.commonCodeVal}</td>
-                <td><a href="/commu/detail/${commu.commuId}">${commu.commuTitle}</a></td>
-                <td>${commu.commuMberId}</td>
-                <td>${commu.commuReadCnt}</td>
-                <td>${commu.commuRegistDate}</td>
-            </tr>
-        `;
+                <tr class="commu-row" data-commu-id="${commu.commuId}">
+                    <td>${commu.commuId}</td>
+                    <td>${commu.commonCodeVal}</td>
+                    <td><a href="/commu/detail/${commu.commuId}">${commu.commuTitle}</a></td>
+                    <td>${commu.commuMberId}</td>
+                    <td>${commu.commuReadCnt}</td>
+                    <td>${commu.commuRegistDate}</td>
+                </tr>
+            `;
 		});
-
-		// 이전에 표시된 게시물 목록을 지우고 새로운 목록으로 대체합니다.
 		$('tbody').empty().append(postListHtml);
 	}
-
-	
-
 });
+

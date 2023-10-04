8715b4c9da51911492ee7bf86cfdb0a73e9c1b76
wxxk <dwde22@gmail.com>
2023-10-04 11:40:21 +0900

    fix : cntnt scroll error

--- K-Content/src/main/resources/static/js/content/content.js
+++ K-Content/src/main/resources/static/js/content/content.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
+	var cateValue = 'All'; // 초기값 설정
+	
 	$.ajax({
 		url: "/user/content",
 		type: "GET",
@@ -7,10 +9,28 @@ $(document).ready(function() {
 			layout.append(result);
 		}
 	})
+	
+	var start = 1;
+	var end = 15;		
+		
+	$(window).scroll(function() {
+		if ( Math.round($(window).scrollTop()) == $(document).height() - $(window).height() ) {
+			start = end + 1;
+			end = start + 4;
+			$.ajax({
+				type: 'GET',
+				url: '/content/scroll',
+				data: {start : start, end : end, cate: cateValue},
+				success: function(result) {
+					change(result);
+				}
+			}) 
+		}
+	})
 
 	$(".cate").click(function() {
-		var cateValue = this.getAttribute('data-maincate-value');
-
+		cateValue = this.getAttribute('data-maincate-value');
+		
 		if (cateValue == null) {
 			cateValue = 'All';
 		}
@@ -58,3 +78,26 @@ function cntntDetail(cntntId) {
 	document.getElementById('contentDetail').submit();
 }
 
+
+function change(result){
+	for (var i = result.contentList.length - 1; i >= 0; i--) {
+		/* console.log(data.contentList[i].cntntThumnail); */
+		var Thumnail = result.contentList[i].cntntThumnail;
+		var Title = result.contentList[i].cntntTitle;
+		var cntntId = result.contentList[i].cntntId;
+		var scroll = `
+			  <img src="`+ Thumnail + `" class="card-img-top" alt="">
+			  <div class="card-body">
+			    <p class="card-title card-text text-center">` + Title + `</p>
+			  </div>
+		`;
+		
+		var divEl = document.createElement("div");
+		divEl.setAttribute("class", "card mx-3 my-3");
+		divEl.setAttribute("style", "width: 18rem;");
+		divEl.setAttribute("cntntId", cntntId);
+		divEl.setAttribute("onclick", "cntntDetail(this.getAttribute('cntntId'))");
+		divEl.innerHTML = scroll;
+		document.querySelector("#contentsList").appendChild(divEl);
+	}
+}

dc8957f683f9448604fec13c02a39f5f0b2c230d
wxxk <dwde22@gmail.com>
2023-10-12 11:49:56 +0900

    feat : user inqury sweet alert

--- K-Content/src/main/resources/static/js/inqury/inquryList.js
+++ K-Content/src/main/resources/static/js/inqury/inquryList.js
@@ -1,58 +1,98 @@
-$(function(){		
+$(function() {
 	const exampleModal = document.getElementById('exampleModal')
-	 	
-	if (exampleModal) {
-	  	exampleModal.addEventListener('show.bs.modal', event => {
 
-		    const button = event.relatedTarget;
-		    const inqryId = button.getAttribute('data-bs-whatever');
-	
-		    const modalBodyInput = exampleModal.querySelector(".modal-body input[name='inqryId']");
+	if (exampleModal) {
+		exampleModal.addEventListener('show.bs.modal', event => {
+
+			const button = event.relatedTarget;
+			const inqryId = button.getAttribute('data-bs-whatever');
+
+			const modalBodyInput = exampleModal.querySelector(".modal-body input[name='inqryId']");
 			const modalBodyInput2 = exampleModal.querySelector(".modal-body input[name='inqryPwd']");
-			
-		    modalBodyInput.value = inqryId;
+
+			modalBodyInput.value = inqryId;
 			modalBodyInput2.value = "";
 		})
 	}
-	
+
 	$(".inqry-row").click(function(e) {
 		const inqryPwd = $(this).attr("data-inqry-pwd");
 		var inqryId = $("#exampleModal").find("input[name='inqryId']").val();
-		if(inqryPwd == null) {
+		if (inqryPwd == null) {
 			window.location.href = "/inqury/detail/" + inqryId;
 		}
 	})
-	
-    $(".submit-btn").click(function(e) {
-        e.preventDefault();
+
+	$("#userPassword").submit(function(e) {
+		e.preventDefault();
 		var inqryId = $("#exampleModal").find("input[name='inqryId']").val();
-  		var enteredPwd = $("#exampleModal").find("input[name='inqryPwd']").val();
+		var enteredPwd = $("#exampleModal").find("input[name='inqryPwd']").val();
+
+		if (enteredPwd === "") {
+			Swal.fire({
+				title: '비밀번호를 입력해주세요',
+				icon: 'warning',
+
+				confirmButtonColor: '#3085d6', // confrim 버튼 색깔 지정
+				confirmButtonText: '확인',
+
+			});
+		} else {
+			$.ajax({
+				url: "/inqury/check-password",
+				method: "post",
+				data: {
+					inqryId: inqryId,
+					enteredPwd: enteredPwd
+				},
+				success: function(data) {
+					var inqryId = $("#exampleModal").find("input[name='inqryId']").val();
+					window.location.href = "/inqury/detail/" + inqryId;
+				},
+				error: function() {
+					Swal.fire({
+						title: '비밀번호가 일치하지 않습니다.',
+						icon: 'warning',
+
+						confirmButtonColor: '#3085d6', // confrim 버튼 색깔 지정
+						confirmButtonText: '확인',
+
+					});
+				}
+			});
+		}
+	})
+
+	/*$(".submit-btn").click(function(e) {
+		e.preventDefault();
+		var inqryId = $("#exampleModal").find("input[name='inqryId']").val();
+			var enteredPwd = $("#exampleModal").find("input[name='inqryPwd']").val();
 		
-        if (enteredPwd === "") {
+		if (enteredPwd === "") {
 			alert("비밀번호를 입력하세요.");
 			return;
 		}		
 		
-        $.ajax({
-            url: "/inqury/check-password",
-            method: "post",
-            data: {
-                inqryId: inqryId,
-                enteredPwd: enteredPwd
-            },
-            success: function(data) {
+		$.ajax({
+			url: "/inqury/check-password",
+			method: "post",
+			data: {
+				inqryId: inqryId,
+				enteredPwd: enteredPwd
+			},
+			success: function(data) {
 				var inqryId = $("#exampleModal").find("input[name='inqryId']").val();
-                window.location.href = "/inqury/detail/" + inqryId;
-            },
-            error: function() {
-            	alert("비밀번호가 일치하지 않습니다.");
-            }
-        });
-    });
+				window.location.href = "/inqury/detail/" + inqryId;
+			},
+			error: function() {
+				alert("비밀번호가 일치하지 않습니다.");
+			}
+		});
+	});*/
 
 	$(".selpage").click(function() {
 		var nowPage = $(this).data("selpage");
-		
+
 		$.ajax({
 			url: "/inqury/" + nowPage,
 			method: "get",
@@ -63,12 +103,12 @@ $(function(){
 			}
 		})
 	})
-	
+
 	$(".prepage").click(function() {
 		var prePage = $(this).data("prepage") - 1;
-		
+
 		$.ajax({
-			url : "/inqury/" + prePage,
+			url: "/inqury/" + prePage,
 			method: "get",
 			seccess: function(data) {
 				let layout = $(".layout");
@@ -77,12 +117,12 @@ $(function(){
 			}
 		})
 	})
-	
+
 	$(".nexpage").click(function() {
 		var nexPage = $(this).data("nexpage") + 1;
-		
+
 		$.ajax({
-			url : "/inqury/" + prePage,
+			url: "/inqury/" + prePage,
 			method: "get",
 			seccess: function(data) {
 				let layout = $(".layout");
--- K-Content/src/main/resources/static/js/inqury/inquryUpdate.js
+++ K-Content/src/main/resources/static/js/inqury/inquryUpdate.js
@@ -1,3 +1,36 @@
-/**
- * 
- */
+$(document).ready(function() {
+    $("#userUpdate").submit(function(event) {
+        event.preventDefault();
+
+        var title = $("#title").val();
+        var cntnt = $("#cntnt").val();
+
+        if (title.trim() === "") {
+            Swal.fire({
+                title: '제목을 입력해주세요',
+                icon: 'warning',
+                confirmButtonText: '확인',
+            });
+        } else if (cntnt.trim() === "") {
+            Swal.fire({
+                title: '내용을 입력해주세요',
+                icon: 'warning',
+                confirmButtonText: '확인',
+            });
+        } else {
+            Swal.fire({
+                title: '수정하시겠습니까?',
+                icon: 'question',
+                showCancelButton: true,
+                cancelButtonColor: '#d33',
+                confirmButtonText: '확인',
+                cancelButtonText: '취소',
+				reverseButtons: true, // 버튼 순서 거꾸로
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    this.submit();
+                }
+            });
+        }
+    });
+});
